0277ce4e8905e897d8b2ba79a8d2320f341a275c
achuchulev <42144722+achuchulev@users.noreply.github.com>
2019-02-15 14:01:23 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "token" {}
+variable "oauth" {}
+variable "tfe_organization" {
+    default = "atanasc"
+}
+variable "aws_account" {
+  description = "AWS Account Name"
+}
+variable "tfe_team" {}
+variable "tfe_team_access" {
+  default = "admin"
+}
+
+variable "tfe_version" {
+  default = "0.11.11"
+}
+variable "workspace_name" {}
+variable "vcs_repo_ident" {}
+variable "repo_working_dir" {}
+variable "workspace_type" {
+  description = "Valid types are 'repo' and 'cli'"
+}
