ed3191c2bcc0f7aee2e1ed400674cd8eebc097ec
achuchulev <42144722+achuchulev@users.noreply.github.com>
2019-02-15 16:14:28 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "tfe_repo_workspace_id" {
   description = "Workspace ID for Repo-based workspace"
   value = "${element(concat(tfe_workspace.repo.*.id, list("")), 0)}"
-  #depends_on = "${tfe_workspace.repo}"
+  depends_on = "${tfe_workspace.repo}"
 }
 
 output "tfe_cli_workspace_id" {
   description = "Workspace ID for CLI-based workspace"
   value = "${element(concat(tfe_workspace.cli.*.id, list("")), 0)}"
-  #depends_on = "${tfe_workspace.cli}"
+  depends_on = "${tfe_workspace.cli}"
 }
