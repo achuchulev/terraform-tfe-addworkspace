4fbd40660b11a06783c74d06fc5357423c5a7465
achuchulev <42144722+achuchulev@users.noreply.github.com>
2019-02-15 14:00:13 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,39 @@
+provider "tfe" {
+  hostname = "app.terraform.io"
+  token    = "${var.token}"
+}
+
+resource "tfe_workspace" "repo" {
+  count               = "${var.workspace_type == "repo" ? 1 : 0}"
+  name                = "${var.workspace_name}"
+  organization        = "${var.tfe_organization}"
+  working_directory   = "${var.repo_working_dir}"
+  terraform_version   = "${var.tfe_version}"
+  queue_all_runs      = false
+  vcs_repo {
+      identifier      = "${var.vcs_repo_ident}"
+      oauth_token_id  = "${var.oauth}"
+  }
+}
+
+resource "tfe_workspace" "cli" {
+  count               = "${var.workspace_type == "cli" ? 1 : 0}"
+  name                = "${var.workspace_name}"
+  organization        = "${var.tfe_organization}"
+  working_directory   = "${var.repo_working_dir}"
+  terraform_version   = "${var.tfe_version}"
+  queue_all_runs      = false
+}
+
+resource "tfe_team_access" "workspace" {
+  access              = "${var.tfe_team_access}"
+  team_id             = "${var.tfe_team}"
+  workspace_id        = "${var.workspace_type == "repo" ? "${tfe_workspace.repo.id}" : "${tfe_workspace.cli.id}"}"
+}
+
+resource "tfe_variable" "info_deploy" {
+  key           = "info_deploy"
+  value         = "TFE"
+  category      = "terraform"
+  workspace_id  = "${var.workspace_type == "repo" ? "${tfe_workspace.repo.id}" : "${tfe_workspace.cli.id}"}"
+}
